a913f4953b29d61c0e883b2589d61104d1b51239
fionn mcgoldrick <fionnmc123@gmail.com>
2025-11-05 17:09:23 +0000

    add polygon to box.getChildren in AppWindow class. add draw method in constructor of PolyPanel to draw on call. lab finished.

--- src/ie/atu/sw/AppWindow.java
+++ src/ie/atu/sw/AppWindow.java
@@ -121,6 +121,7 @@ public class AppWindow extends Application {
 		box.getChildren().add(getFileChooserPane(stage)); //Add the sub tree to the main tree
 		box.getChildren().add(getTableView()); //Add the sub tree to the main tree
 		box.getChildren().add(toolBar); //Add the sub tree to the main tree
+		box.getChildren().add(new PolyPanel());
 
 		// Display the window
 		stage.show();
--- src/ie/atu/sw/PolyPanel.java
+++ src/ie/atu/sw/PolyPanel.java
@@ -1,6 +1,7 @@
 package ie.atu.sw;
 
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 
 public class PolyPanel extends HBox {
@@ -9,13 +10,18 @@ public class PolyPanel extends HBox {
 
 	public PolyPanel() {
 		super();
+		draw();
 		super.getChildren().add(p);
 		this.setOnMouseClicked(e -> draw());
 		// TODO Auto-generated constructor stub
 	}
 
 	public void draw() {
-		Polygon p = new Polygon();
+		
+		p.setStroke(Color.color(Math.random(), Math.random(), Math.random()));
+		
+		p.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+
 		for (int i = 0; i < 6; i++) {
 			p.getPoints().addAll(new Double[] { (100 + 50 * Math.cos(i * 2 * Math.PI / 6)),
 					(100 + 50 * Math.sin(i * 2 * Math.PI / 6)) });
